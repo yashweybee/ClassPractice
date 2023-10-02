ccfe57da435bfc2ca9ea0ac03c9153a90d29b57a
yashweybee <yashparmar@weybee.in>
2023-10-02 10:48:34 +0530

    Add project files.

---
+++ ClassPractice/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            Student s2 = new Student();
+            //s1.id = 100;
+            //s1.name = "abcd";
+
+            s1.init(1, "Yash");
+            s2.init(2, "Ravi");
+            s1.display();
+            s2.display();
+            
+            Console.ReadLine();
+        }
+    }
+
+    public class Student
+    {
+        public int id;
+        public string name;
+    public void init(int id , string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    
+        public void display()
+        {
+            Console.WriteLine(this.id + " "+ this.name);
+        }
+    }
+}
